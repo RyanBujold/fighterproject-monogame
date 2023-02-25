3ed6b47a446d17b56803c866a8ccffe44b7f6680
RyanBujold <55700224+RyanBujold@users.noreply.github.com>
2023-02-25 16:18:51 -0500

    added fighter project base code
    added code

---
+++ FighterProject/FighterProject/Objects/CollisionBox.cs
@@ -0,0 +1,98 @@
+﻿using FighterProject.Entities.Characters;
+using Microsoft.Xna.Framework;
+
+namespace FighterProject.Library {
+    /// <summary>
+    /// A CollisionBox that represents a rectangle used for
+    /// various collision detection.
+    /// </summary>
+    class CollisionBox {
+
+        /// <summary>
+        /// The offset to draw the collision box at.
+        /// </summary>
+        public Vector2 Offset { get; set; }
+
+        /// <summary>
+        /// A rectangle representing the collision box.
+        /// </summary>
+        public Rectangle Box { get; private set; }
+
+        /// <summary>
+        /// The width of the collision box.
+        /// </summary>
+        public int Width { get; private set; }
+
+        /// <summary>
+        /// The height of the collision box.
+        /// </summary>
+        public int Height { get; private set; }
+
+        /// <summary>
+        /// A Collision Box object.
+        /// </summary>
+        /// <param name="Xoffset">The X offset.</param>
+        /// <param name="Yoffset">The Y offset.</param>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        /// <param name="damage">The attack damage.</param>
+        public CollisionBox(float Xoffset, float Yoffset, int width, int height) {
+            Offset = new Vector2(Xoffset, Yoffset);
+            Box = new Rectangle(0, 0, width, height);
+            Width = width;
+            Height = height;
+        }
+
+        /// <summary>
+        /// Scale the collision box width and height.
+        /// </summary>
+        /// <param name="scale">The scale.</param>
+        /// <returns>The scaled collision box.</returns>
+        public CollisionBox ScaleSize(float scale) {
+            return new CollisionBox(Offset.X, Offset.Y, Width * (int)scale, Height * (int)scale);
+        }
+
+        /// <summary>
+        /// Scale the collision box offset.
+        /// </summary>
+        /// <param name="scale">The scale.</param>
+        /// <returns>The scaled collision box.</returns>
+        public CollisionBox ScaleOffset(float scale) {
+            return new CollisionBox(Offset.X * scale, Offset.Y * scale, Width, Height);
+        }
+
+        /// <summary>
+        /// Check if attacker's collision box collided with defender's collision box.
+        /// </summary>
+        /// <param name="atkDrawPos">The attacker's draw position.</param>
+        /// <param name="defDrawPos">The defender's draw position.</param>
+        /// <param name="atkBox">The attacker's collision box.</param>
+        /// <param name="defBox">The defender's collision box.</param>
+        /// <param name="atkScale">The attacker's scale.</param>
+        /// <param name="defScale">The defener's scale.</param>
+        /// <returns>True if the CollisionBoxes have collided. False otherwise.</returns>
+        public static bool DidCollide(Vector2 atkDrawPos, Vector2 defDrawPos, CollisionBox atkBox, CollisionBox defBox, float atkScale, float defScale) {
+            // Return if the is no hitbox.
+            if(atkBox.Width == 0 && atkBox.Height == 0){ return false; }
+            if(defBox.Width == 0 && defBox.Height == 0){ return false; }
+
+            // Setup variables
+            float atkLeft = atkDrawPos.X + atkBox.Offset.X;
+            float atkRight = atkDrawPos.X + atkBox.ScaleSize(atkScale).Width + atkBox.Offset.X;
+            float atkBottom = atkDrawPos.Y + atkBox.ScaleSize(atkScale).Height + atkBox.Offset.Y;
+            float atkTop = atkDrawPos.Y + atkBox.Offset.Y;
+
+            float defLeft = defDrawPos.X + defBox.Offset.X;
+            float defRight = defDrawPos.X + defBox.ScaleSize(defScale).Width + defBox.Offset.X;
+            float defBottom = defDrawPos.Y + defBox.ScaleSize(defScale).Height + defBox.Offset.Y;
+            float defTop = defDrawPos.Y + defBox.Offset.Y;
+
+            // Check collision
+            if (atkRight >= defLeft && atkLeft <= defRight && atkBottom >= defTop && atkTop <= defBottom) {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ FighterProject/FighterProject/Objects/Hitbox.cs
@@ -0,0 +1,59 @@
+﻿
+namespace FighterProject.Library {
+
+    /// <summary>
+    /// A hitbox that defines an attack's type and damage.
+    /// </summary>
+    class Hitbox : CollisionBox{
+
+        /* Add attack types. Ex. high attack, low attack, mid attack...*/
+
+        /// <summary>
+        /// The id representing the hitbox group.
+        /// </summary>
+        public int GroupId { get; private set; }
+
+        /// <summary>
+        /// The damage done upon collision.
+        /// </summary>
+        public int Damage { get; private set; }
+
+        /// <summary>
+        /// An empty hitbox.
+        /// </summary>
+        public static Hitbox None = new Hitbox(0, 0, 0, 0, 0);
+
+        /// <summary>
+        /// An attack hitbox.
+        /// </summary>
+        /// <param name="Xoffset">The X offset.</param>
+        /// <param name="Yoffset">The Y offset.</param>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        /// <param name="damage">The attack damage.</param>
+        /// <param name="id">The hitboxe's group id.</param>
+        public Hitbox(int Xoffset, int Yoffset, int width, int height, int damage, int id = 1) : base(Xoffset, Yoffset, width, height) {
+            Damage = damage;
+            GroupId = id;
+        }
+
+        /// <summary>
+        /// Check if hitboxes are the same.
+        /// </summary>
+        /// <param name="obj">The hitbox.</param>
+        /// <returns>True if hitboxes are the same.</returns>
+        public bool Equals(Hitbox col) {        
+            // Don't check the offset because it changes when the player is facing left.
+            if (Width == col.Width &&
+               Height == col.Height &&
+               Damage == col.Damage)
+                return true;
+            else
+                return false;
+
+        }
+
+        /* Add collision box definition that takes an animation to transition to an animation on hit?*/
+
+    }
+}
